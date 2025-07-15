8258c65d6fe0a3ae3f21b9bc9716b6c78b63cc25
Khulud Mohammed <152332677+Kuludmo@users.noreply.github.com>
2025-07-15 22:30:06 +0300

    Time: 37 ms (58.95%) | Memory: 17.6 MB (67.00%) - LeetSync

---
+++ 1970-sorting-the-sentence/sorting-the-sentence.py
@@ -0,0 +1,25 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+         def isnumber(word):
+            number=''
+            for i in word:
+                 if i.isdigit():
+                    number += i
+            return number
+         array=s.split()
+         array=sorted(array, key=isnumber)
+         ans=[]
+         for i in array:
+            x=i[:-1]
+            ans.append(x)
+         result=' '.join(ans)
+         return result
+         
+         
+
+
+                    
+          #return number
+         #array = s.split()
+         #result = isnumber(array)
+         #print(sorted(result))
