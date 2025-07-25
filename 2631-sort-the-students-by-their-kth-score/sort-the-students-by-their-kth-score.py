8de91085ec4fd1c58f4004cba712e0cd037b6906
Khulud Mohammed <152332677+Kuludmo@users.noreply.github.com>
2025-07-25 19:43:51 +0300

    Time: 7 ms (22.75%) | Memory: 22.8 MB (24.80%) - LeetSync

---
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:
+        score.sort(key=lambda x:x[k], reverse=True)
+        print(score)
+        return score
+     
+        
+    
+            
