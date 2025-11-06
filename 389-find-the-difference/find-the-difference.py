1db93418d4319abc2c1cabc3da56f6d3ddb31bf5
Khulud Mohammed <152332677+Kuludmo@users.noreply.github.com>
2025-11-06 20:56:23 +0300

    Time: 0 ms (100.00%) | Memory: 12.5 MB (23.97%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        for ch in t:
+            if t.count(ch) == s.count(ch):
+                continue
+            elif t.count(ch) != s.count(ch):
+                return ch
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        
